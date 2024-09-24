3cde3a8aaf8e42371258eae80784fbe461b5250a
Romuald Brillout <git@brillout.com>
2024-09-24 21:27:54 +0200

    wip

--- test/middleware-setting/express-entry.ts
+++ test/middleware-setting/express-entry.ts
@@ -1,23 +1,24 @@
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-import { vikeHandler } from './server/vike-handler'
-import { telefuncHandler } from './server/telefunc-handler'
-import { createHandler } from '@universal-middleware/express'
+import { createMiddleware } from '@universal-middleware/express'
 import express from 'express'
+import { getPageFilesAllSafe } from 'vike/__internal'
+import type { Middleware } from './pages/Middleware'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const root = __dirname
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
 const hmrPort = process.env.HMR_PORT ? parseInt(process.env.HMR_PORT, 10) : 24678
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default (await startServer()) as unknown
 
 async function startServer() {
   const app = express()
 
-  if (process.env.NODE_ENV === 'production') {
+  if (isProduction) {
     app.use(express.static(`${root}/dist/client`))
   } else {
     // Instantiate Vite's development server and integrate its middleware to our server.
@@ -33,18 +34,23 @@ async function startServer() {
     app.use(viteDevMiddleware)
   }
 
-  app.post('/_telefunc', createHandler(telefuncHandler)())
-
-  /**
-   * Vike route
-   *
-   * @link {@see https://vike.dev}
-   **/
-  app.all('*', createHandler(vikeHandler)())
+  const { pageConfigs } = await getPageFilesAllSafe(isProduction) // We recommend, whenever possible, using import.meta.env.PROD instead
+  const middlewares: Middleware[] = (pageConfigs[0].configValues.middleware.value as any).flat(Infinity)
+  middlewares.sort((m1, m2) => getOrder(m1.order) - getOrder(m2.order))
+  middlewares.forEach((middlewareSpec) => {
+    const middleware = middlewareSpec.value
+    app.all('*', createMiddleware(() => middleware)())
+  })
 
   app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
   })
 
   return app
+}
+
+function getOrder(order: Middleware['order']): number {
+  if (order === 'post') return 100
+  if (order === 'pre') return -100
+  return order
 }
